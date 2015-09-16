cbf9f8f8be0666f239a014f24a66d1002545fe26
paulvollmer <paul.vollmer@fh-potsdam.de>
2015-09-16 07:49:07 +0200

    Create LICENSE file [ci-skip]

--- default.go
+++ default.go
@@ -1,3 +1,7 @@
+// Copyright 2015 myles.systems. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package datautil
 
 // Default store the default data for a Load call.
