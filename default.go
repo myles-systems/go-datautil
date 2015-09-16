9d6eacc06b826c0aeb886880c841147a307f35ed
paulvollmer <paul.vollmer@fh-potsdam.de>
2015-09-16 07:00:48 +0200

    Added default.go docs [ci-skip]

--- default.go
+++ default.go
@@ -1,14 +1,17 @@
 package datautil
 
+// Default store the default data for a Load call.
 type Default struct {
 	Data []byte
 }
 
+// DefaultString set a string to a Default struct and return it.
 func DefaultString(d string) Default {
 	tmp := []byte(d)
 	return Default{tmp}
 }
 
+// DefaultBytes set a byte array to a Default struct and return it.
 func DefaultBytes(d []byte) Default {
 	return Default{d}
 }
